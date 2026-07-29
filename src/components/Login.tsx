12beb32ee54045bef2dbed2a1283431494c069c2
alan araújo <ag23672311@gmail.com>
2026-07-28 21:52:38 -0300

    feat: adiciona indicador de loading e card de login estilizado

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -12,6 +12,7 @@ const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 const[error, setError] = useState('');
 const[successMessage, setSuccessMessage] = useState('');
+const [loading, setLoading] = useState(false);
 
 const { login } = useContext(AuthContext);
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
@@ -20,6 +21,8 @@ const handleLoSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setError('');
     setSuccessMessage('');
+    setLoading(true);
+
 
     try {
         if (isRegistering) {
@@ -36,11 +39,14 @@ const handleLoSubmit = async (e: React.FormEvent) => {
     } 
 }catch (err: any) {
         setError(err.response?.data?.message || 'Erro ao processar a solicitação. Verifique suas credenciais.');
+    } finally {
+        setLoading(false);
     }
     };
 
     return (
         <div className='Login-container'>
+            <div className="login-card">
         <h1>{isRegistering ? 'Criar Conta' : 'Login'}</h1>
 
 {error && <div className="error-message" style={{ color: 'red', marginBottom: '10px' }}> {error}</div>}
@@ -72,8 +78,8 @@ onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
 required
 />
 
-<button type="submit" className="login-btn">
-    {isRegistering ? 'Cadastrar' : 'Entrar'}
+<button type="submit" className="login-btn" disabled={loading}>
+    {loading ? 'Carregando...' : (isRegistering ? 'Cadastrar' : 'Entrar')}
 </button>
 </form>
 
@@ -88,6 +94,7 @@ required
 >
     {isRegistering ? 'Já tem uma conta? Faça login' : 'Não tem uma conta? Crie agora'}
     </button>
+    </div>
     </div>
 </div>
     );
