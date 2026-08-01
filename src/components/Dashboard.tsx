85f3193034529362e283a12bda2726a398bb3e38
alan araújo <ag23672311@gmail.com>
2026-07-31 23:08:49 -0300

    adicionando sonoros efeitos!

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -33,7 +33,7 @@ export function Dashboard() {
   const progress = Math.min(Math.round((balance > 0 ? balance / 5000 * 100 : 0)), 100);
   
   const playSound = () => {
-  const audio = new Audio('/apexfin/public/mrstokes302-success-videogame-sfx-mrstokes302-423626.mp3');
+  const audio = new Audio('/apexfin/public/qw.mp3');
   audio.play().catch(err => {
     console.log("Erro ao tocar áudio:", err);
   });
@@ -64,7 +64,7 @@ export function Dashboard() {
       try {
         await api.delete(`transactions/${id}`);
 
-const audio = new Audio('/apexfin/public/freesound_community-mag-remove-92075.mp3');
+const audio = new Audio('/apexfin/public/qe.mp3');
 audio.play().catch(err => console.log(err));
 
         loadTransactions();
